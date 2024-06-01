1414a6fd5aa6de4fd96dca35996f5649024909a9
dollyaggarwal123 <dolly.aggarwal@oodles.io>
2024-06-01 21:41:47 +0530

    debugging

--- Socket/socket.js
+++ Socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:3000/chatapp",
     metthods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
