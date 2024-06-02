d0760c2dc3dffb9ca53e80f12e6fb62cc75f897f
dollyaggarwal123 <dolly.aggarwal@oodles.io>
2024-06-02 17:11:32 +0530

    debugging

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.options("*", cors());
+// app.options("*", cors());
 
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
