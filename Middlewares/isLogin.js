a9cd4b2a584e515b3449d0f0288daa507355ace4
dollyaggarwal123 <dolly.aggarwal@oodles.io>
2024-06-03 10:59:16 +0530

    debugging errors

--- Middlewares/isLogin.js
+++ Middlewares/isLogin.js
@@ -3,7 +3,8 @@ import User from "../Models/userModels.js";
 
 const isLogin = async (req, res, next) => {
   try {
-    const token = req.cookies.jwt;
+    const token = req.cookies?.jwt;
+
     if (!token)
       return res
         .status(500)
