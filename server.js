2cd7e2e8b686b089e329ed16d12cf86a0505066a
dollyaggarwal123 <dolly.aggarwal@oodles.io>
2024-06-04 20:23:35 +0530

    chnged url

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
