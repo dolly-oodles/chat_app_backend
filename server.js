dcb47b3f8d8e99d2ef5f06ef0ea6ea1505c33ce5
dollyaggarwal123 <dolly.aggarwal@oodles.io>
2024-06-04 20:05:12 +0530

    chnged url

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://conn-action-dashboard.netlify.app",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
