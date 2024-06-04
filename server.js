792a9095987639b9bf3f8de0ef5d419e9f9b953e
dollyaggarwal123 <dolly.aggarwal@oodles.io>
2024-06-04 13:05:58 +0530

    changed frontend hosted url

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://conn-action-dashboard.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
